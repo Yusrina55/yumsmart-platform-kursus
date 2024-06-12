9fad2782c93290d9aaac5f89c229518dc23a7103
Astri Auliya <astriauliya5@gmail.com>
2024-06-13 03:50:08 +0700

    index.php

--- views/index.php
+++ views/index.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YUMSMART</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Welcome to YUMSMART! Silakan Login!</h1>
-    <ul>
-        <li><a href="../views/ortu/masuk.html">Siswa</a></li>
-      	<li><a href="../views/tutor/masuk-tutor.html">Tutor</a></li>
-      	<li><a href="../views/admin/masuk-admin.html">Admin</a></li>
+
+<body class="bg-yellow-400 text-gray-800 h-screen flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold mb-4 text-center">Welcome to YUMSMART! Silakan Login!</h1>
+    <ul class="space-y-4">
+        <li>
+            <a href="../views/ortu/masuk.html"
+                class="block bg-black hover:bg-yellow-500 text-yellow-400 font-bold py-2 px-4 rounded text-center">Siswa</a>
+        </li>
+        <li>
+            <a href="../views/tutor/masuk-tutor.html"
+                class="block bg-black hover:bg-yellow-500 text-yellow-400 font-bold py-2 px-4 rounded text-center">Tutor</a>
+        </li>
+        <li>
+            <a href="../views/admin/masuk-admin.html"
+                class="block bg-black hover:bg-yellow-500 text-yellow-400 font-bold py-2 px-4 rounded text-center">Admin</a>
+        </li>
     </ul>
 </body>
+
 </html>
